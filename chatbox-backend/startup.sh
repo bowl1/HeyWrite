bff8bbf08bdeae7d8942740c87825c13863682a0
bowl1 <bowenivy0@gmail.com>
2025-06-05 15:37:00 +0200

    update startup

--- chatbox-backend/startup.sh
+++ chatbox-backend/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install -r requirements.txt
+echo ">>> Installing requirements..."
+pip install -r requirements.txt --verbose
+echo ">>> Starting server..."
 uvicorn main:app --host=0.0.0.0 --port=8000
